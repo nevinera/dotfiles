05bf9e7ee81c07221a969d5752bdaa5895a96436
Eric Mueller <emueller@emcien.com>
2014-11-04 06:59:25 -0500

    add a basic install script to link files into place

---
+++ install.sh
@@ -0,0 +1,5 @@
+ln -s $HOME/.vim/bashrc $HOME/.bashrc
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
+ln -s $HOME/.vim/bash_aliases $HOME/.bash_aliases
+ln -s $HOME/.vim/bash_prompt $HOME/.bash_prompt
+ln -s $HOME/.vim/gitconfig $HOME/.gitconfig

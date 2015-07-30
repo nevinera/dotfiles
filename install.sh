9331e0680f5cbdcd4f20ff3c6d8e7bf4f4ae9746
Eric Mueller <emueller@emcien.com>
2015-07-30 09:34:15 -0400

    add git completion file

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -s $HOME/.vim/vimrc $HOME/.vimrc
 ln -s $HOME/.vim/bash_aliases $HOME/.bash_aliases
 ln -s $HOME/.vim/bash_prompt $HOME/.bash_prompt
 ln -s $HOME/.vim/gitconfig $HOME/.gitconfig
+ln -s $HOME/.vim/git-completion $HOME/.git-completion

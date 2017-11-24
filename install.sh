19e76c74a6fc59b1b5207cec1f3d520b39708346
Eric Mueller <nevinera@gmail.com>
2017-11-24 16:35:25 -0500

    install slate file too

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s $HOME/.vim/bash_aliases $HOME/.bash_aliases
 ln -s $HOME/.vim/bash_prompt $HOME/.bash_prompt
 ln -s $HOME/.vim/gitconfig $HOME/.gitconfig
 ln -s $HOME/.vim/git-completion $HOME/.git-completion
+ln -s $HOME/.vim/slate $HOME/.slate

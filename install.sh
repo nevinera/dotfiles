bc88d6cd1eca566e85c7bc4759ca5e2a07e97087
Eric Mueller <emueller@emueller-mnqp7.lan>
2022-06-25 11:33:52 -0400

    drop everything, we're starting over

--- install.sh
+++
@@ -1,7 +0,0 @@
-ln -s $HOME/src/dotfiles/bashrc $HOME/.bashrc
-ln -s $HOME/src/dotfiles/vimrc $HOME/.vimrc
-ln -s $HOME/src/dotfiles/bash_aliases $HOME/.bash_aliases
-ln -s $HOME/src/dotfiles/bash_prompt $HOME/.bash_prompt
-ln -s $HOME/src/dotfiles/gitconfig $HOME/.gitconfig
-ln -s $HOME/src/dotfiles/git-completion $HOME/.git-completion
-ln -s $HOME/src/dotfiles/slate $HOME/.slate
